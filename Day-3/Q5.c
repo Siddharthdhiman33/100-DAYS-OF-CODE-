2dd00f1cf8e82c04ab7e8670450bf35e6b155cae
Siddharthdhiman33 <dhimansiddharth4@gmail.com>
2025-09-04 23:50:05 +0530

    Day-03

---
+++ Day-3/Q5.c
@@ -0,0 +1,11 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+#include <stdio.h>
+int main() 
+{
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("%f°C is equal to %f°F\n", celsius, fahrenheit);
+    return 0;
+}
